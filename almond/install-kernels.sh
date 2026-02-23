ceeabcfb74c0dead3a995f68945f589e97d7a306
afakari <amirhossein.fakari@digikala.com>
2026-02-23 19:49:53 +0330

    chore: add jupyter config, add metals, add plugins, add toree magics

--- almond/install-kernels.sh
+++ almond/install-kernels.sh
@@ -1,25 +1,63 @@
 #!/usr/bin/env bash
 set -eu
 
+JAVA_REFS=(
+  "--add-opens=java.base/java.lang=ALL-UNNAMED"
+  "--add-opens=java.base/java.lang.invoke=ALL-UNNAMED"
+  "--add-opens=java.base/java.lang.reflect=ALL-UNNAMED"
+  "--add-opens=java.base/java.io=ALL-UNNAMED"
+  "--add-opens=java.base/java.net=ALL-UNNAMED"
+  "--add-opens=java.base/java.nio=ALL-UNNAMED"
+  "--add-opens=java.base/java.util=ALL-UNNAMED"
+  "--add-opens=java.base/java.util.concurrent=ALL-UNNAMED"
+  "--add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED"
+  "--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED"
+  "--add-opens=java.base/sun.nio.ch=ALL-UNNAMED"
+  "--add-opens=java.base/sun.security.action=ALL-UNNAMED"
+  "-Djdk.reflect.useDirectMethodHandle=false"
+)
+
 [ -z "$SCALA_VERSIONS" ] && { echo "SCALA_VERSIONS is empty" ; exit 1; }
 [ -z "$ALMOND_VERSION" ] && { echo "ALMOND_VERSION is empty" ; exit 1; }
+
 for SCALA_FULL_VERSION in ${SCALA_VERSIONS}; do
-  # remove patch version
   SCALA_MAJOR_VERSION=${SCALA_FULL_VERSION%.*}
-  # remove all dots for the kernel id
-  SCALA_MAJOR_VERSION_TRIMMED=$(echo ${SCALA_MAJOR_VERSION} | tr -d .)
-  echo Installing almond ${ALMOND_VERSION} for Scala ${SCALA_FULL_VERSION}
-  EXTRA_ARGS=()
-  if [[ ${ALMOND_VERSION} == *-SNAPSHOT ]]; then
-    EXTRA_ARGS+=('--standalone')
-  fi
+  SCALA_MAJOR_VERSION_TRIMMED=$(echo "${SCALA_MAJOR_VERSION}" | tr -d .)
+  KERNEL_ID="scala${SCALA_MAJOR_VERSION_TRIMMED}"
+
+  echo "Installing almond ${ALMOND_VERSION} for Scala ${SCALA_FULL_VERSION}"
+
   coursier bootstrap \
       -r jitpack \
       -i user -I user:sh.almond:scala-kernel-api_${SCALA_FULL_VERSION}:${ALMOND_VERSION} \
       sh.almond:scala-kernel_${SCALA_FULL_VERSION}:${ALMOND_VERSION} \
       --default=true --sources \
-      -o almond ${EXTRA_ARGS[@]}
-  ./almond --install --log info --metabrowse --id scala${SCALA_MAJOR_VERSION_TRIMMED} --display-name "Scala ${SCALA_MAJOR_VERSION}"
+      -o almond
+
+  ./almond --install \
+    --log info \
+    --metabrowse \
+    --id "$KERNEL_ID" \
+    --display-name "Scala ${SCALA_MAJOR_VERSION}" \
+    --toree-compatibility \
+
   rm -f almond
+
+  KERNEL_JSON="/home/jovyan/.local/share/jupyter/kernels/${KERNEL_ID}/kernel.json"
+
+  if [ -f "$KERNEL_JSON" ]; then
+     echo "Patching $KERNEL_JSON with Java refs..."
+
+     tmp=$(mktemp)
+     jq --argjson refs "$(printf '%s\n' "${JAVA_REFS[@]}" | jq -R . | jq -s .)" \
+        '.argv = [.argv[0]] + $refs + .argv[1:]' \
+        "$KERNEL_JSON" > "$tmp" && mv "$tmp" "$KERNEL_JSON"
+    else
+     echo "Warning: Could not find $KERNEL_JSON"
+    fi
+
+  coursier bootstrap org.scalameta:metals_2.13:1.6.5 --force-fetch -o /opt/conda/metals -f
+
+ echo '{"LanguageServerManager":{"language_servers":{"metals":{"version":2,"argv":["/opt/conda/metals"],"languages":["scala"],"mime_types":["text/x-scala"]}}}}' >> /opt/conda/etc/jupyter/jupyter_server_config.d/metals-ls.json
+
 done
-echo Installation was successful
